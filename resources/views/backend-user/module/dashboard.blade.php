3227fae95831b2bf7385f287cd5b14d19f56df21
Nizar Khan <nizarkhan7071@gmail.com>
2026-04-15 04:58:10 +0800

    add template

---
+++ resources/views/backend-user/module/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('backend-user.layouts.app')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+    <div class="col-span-12">
+        <h1 class="text-2xl font-bold text-gray-800 dark:text-white">Admin Dashboard</h1>
+        <!-- Content dashboard anda di sini -->
+    </div>
+@endsection
